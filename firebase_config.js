f5678d0776314fcd760581f188729f72b5c1f67b
clement <58054176+clm-msch@users.noreply.github.com>
2023-02-21 09:20:07 +0100

    initial commit

---
+++ firebase_config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjQZBoHx-7YC4IHNcXbFLEgwhJhOHSIN8",
+    authDomain: "esd-b2dw-4b4e8.firebaseapp.com",
+    projectId: "esd-b2dw-4b4e8",
+    storageBucket: "esd-b2dw-4b4e8.appspot.com",
+    messagingSenderId: "882462609276",
+    appId: "1:882462609276:web:73ac56b4de3766108d1ef9"
+  };
+  
+  // Initialize Firebase
+  export const firebaseApp = initializeApp(firebaseConfig);
